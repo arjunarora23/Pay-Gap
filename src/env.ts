e641f2d1f2b2de546401f6e83c5b2dfb10e020a8
arjunarora23 <aarora@catalystone.com>
2026-06-24 20:31:37 +0530

    fix: removed from github env

--- src/env.ts
+++ src/env.ts
@@ -4,7 +4,7 @@ export const env = {
   catalystoneClientId: '4db53012-a3f1-49c7-8ed3-1d4a8306f2e6',
   catalystoneRedirectUri: 'https://arjunarora23.github.io/Pay-Gap/callback',
   catalystoneScope: 'profile',
-  catalystoneApiBaseUrl: 'https://api.devtest.catalystone.dev',
+  catalystoneApiBaseUrl: 'https://api.devtest.catalystone.io',
 } as const
 
 export type AppEnv = typeof env
