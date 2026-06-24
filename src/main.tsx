67fff7818448a13e100e4c2ed25cf5931c013d64
arjunarora23 <aarora@catalystone.com>
2026-06-24 14:39:08 +0530

    fix: changes

--- src/main.tsx
+++ src/main.tsx
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <AuthProvider>
         <Routes>
           <Route path="/" element={<App />} />
-          <Route path="/callback" element={<CallbackPage />} />
+          <Route path="/salary-comparison" element={<CallbackPage />} />
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </AuthProvider>
